466f621c68988f3ad1501bfd832d2dee49d8c389
Kimberly Truong <kttruong258@gmail.com>
2024-08-21 14:23:12 -0400

    clean up vestigial code and ignore /bin/Rhistory

--- bin/full_pregnancy.R
+++ bin/full_pregnancy.R
@@ -1,44 +1,42 @@
+# include documentation here 
 
 library(httk)
 library(data.table)
 library(dplyr)
-library(tidyverse)
-library(reshape2)
-library(RColorBrewer)
-
-# typical compartments for individuals 
-maternal_compts <- c('gutlumen', 'gut', 'liver', 'kidney', 'lung', 'ven', 'art', 
-                     'adipose','thyroid', 'rest')
-maternal_states <- paste0('A', maternal_compts)
-maternal_states <- c(maternal_states, 
-                     'Atubules', 'Ametabolized', 'AUC')
-maternal_concs <- paste0("C", maternal_compts[! (maternal_compts %in% c("gutlumen"))])
-
-fetal_compts <- c(maternal_compts[!( maternal_compts %in% c('adipose', 'gutlumen') )], 
-                  "brain")
-fetal_states <- c(paste0('Af', fetal_compts), 'fAUC')
-fetal_concs <- paste0("Cf", fetal_compts)
-
-# all compts by the 2nd/3rd trimesters
-mf.states <- c(maternal_states, fetal_states, "Aplacenta")
-mf.outputs <- c(mf.states, # all possible outputs of fetal_pbtk
-                maternal_concs, 
-                fetal_concs, 
-                "Cplacenta", 
-                "Cplasma", "Aplasma", "Rblood2plasma", 
-                "Cfplasma", "Afplasma", "Rfblood2plasma")
-
-# spell out all possible outputs of 1tri_pbtk
-firsttri.states <- c(maternal_states, "Aconceptus")
-firsttri.outputs <- c(firsttri.states, 
-                      maternal_concs, "Cconceptus", 
-                      "Cplasma", "Aplasma", "Rblood2plasma")
+library(ggplot2)
 
 full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
                            return.units = "amt",
-                           time.course = seq(0,40*7,1/24),
+                           time.course = seq(0,40*7,1/24), # every hour
                            ...) {
   cat("Solving for chemical: ", dtxsid, "\n")
+  
+  maternal_compts <- c('gutlumen', 'gut', 'liver', 'kidney', 'lung', 'ven', 'art', 
+                       'adipose','thyroid', 'rest')
+  maternal_states <- paste0('A', maternal_compts)
+  maternal_states <- c(maternal_states, 
+                       'Atubules', 'Ametabolized', 'AUC')
+  maternal_concs <- paste0("C", maternal_compts[! (maternal_compts %in% c("gutlumen"))]) # no Vgutlumen 
+  
+  fetal_compts <- c(maternal_compts[!( maternal_compts %in% c('adipose', 'gutlumen') )], 
+                    "brain") 
+  fetal_states <- c(paste0('Af', fetal_compts), 'fAUC')
+  fetal_concs <- paste0("Cf", fetal_compts)
+  
+  # all compts by the 2nd/3rd trimesters
+  mf.states <- c(maternal_states, fetal_states, "Aplacenta")
+  mf.outputs <- c(mf.states, # these are amounts (states of fetal_pbtk)
+                  maternal_concs, 
+                  fetal_concs, 
+                  "Cplacenta", 
+                  "Cplasma", "Aplasma", "Rblood2plasma", 
+                  "Cfplasma", "Afplasma", "Rfblood2plasma")
+  
+  # spell out all possible outputs of 1tri_pbtk
+  firsttri.states <- c(maternal_states, "Aconceptus")
+  firsttri.outputs <- c(firsttri.states, # amounts 
+                        maternal_concs, "Cconceptus", 
+                        "Cplasma", "Aplasma", "Rblood2plasma")
   
   # split time.course to the appropriate domain for each model
   t1 = time.course[time.course <= 13*7]
@@ -51,12 +49,10 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
   # track chemical amounts (i.e. state vars of each model) 
   firsttri.out <- solve_1tri_pbtk(dtxsid = dtxsid, 
                                   times = t1, 
-                                  dose = 0,
-                                  # rtol = 1e-7, # default: rtol = 1e-8, these specs are for Ergocalciferol
-                                  # atol = 1e-11, # default: atol = 1e-12
+                                  dose = 0, # initial dose on day 1 
                                   monitor.vars = firsttri.outputs, 
                                   ...)
-  
+
   # initialize vector for "initial.values" input to fetal_pbtk 
   initial.dat <- setNames(rep(0, length(mf.states)), mf.states)
   
@@ -68,7 +64,7 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
   missing.amts <- c(paste0("Af", fetal_compts),
                     "Aplacenta")
   
-  # partition out chemical into fetal compts + placenta assuming a well-mixed conceptus
+  # partition out chemical into fetal compts + placenta based on weights from KfVf
   missing.vols <- str_replace(missing.amts, "^A", "V")
   
   # get volumes from C model implementation
@@ -77,11 +73,6 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
                                times = c(13*7, 13*7+1), # times needs to contain at least 2 values
                                monitor.vars = c(missing.vols, "fhematocrit")
   )
-  
-  # # check that vols.out first row is for day 91 
-  # Af = firsttri.out[ind, "Cconceptus"]*vols.out[1, missing.vols]
-  # names(Af) <- missing.amts 
-  # initial.dat[names(Af)] = Af 
   
   fetal.parms <- parameterize_fetal_pbtk(dtxsid = dtxsid)
   
@@ -102,7 +93,6 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
   KVtotal = KVtotal + vols.out[[1, "fhematocrit"]]*fetal.pcs[["Kfrbc2pu"]]*sum(bV) #RBCs
   KVtotal = KVtotal + + (1-vols.out[[1, "fhematocrit"]])/fetal.parms$Fraction_unbound_plasma_fetus*sum(bV) #plasma 
   
-  # check that vols.out first row is for day 91 
   Af = firsttri.out[ind, "Aconceptus"]*Vf*Kf/KVtotal
   names(Af) <- sub("^V", "A", names(Af))
   initial.dat[names(Af)] = Af 
@@ -129,18 +119,15 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
   
   # get full solution by concatenating 2 outputs
   full_sol <- bind_rows(data.frame(firsttri.out), data.frame(mod.fetal.out))
-  
-  # add initial amts approximated at day 91 from Cconceptus 
-  # always return the solution with calculations and fetal_pbtk solution for day 91
+
+  # add initial.values computed at day 91 from Aconceptus(13)
+  # always return the solution with derived initial conds and fetal_pbtk solution at day 91
   full_sol[ind, c(missing.amts, "fAUC")] = initial.dat[c(missing.amts, "fAUC")]
   
-  # plot all available states (the amts)
+  # plot all states (the amts)
   if (plt == TRUE) {
     
     if (return.units == "amt") {
-      # cols <- c(maternal_states[maternal_states != "AUC"], 
-      #           fetal_states[fetal_states != "fAUC"], 
-      #           "Aconceptus", "Aplacenta")
       
       cols <- c(maternal_states, fetal_states, 
                 "Aconceptus", "Aplacenta")
@@ -155,42 +142,22 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
       out[, ftotal := Afgut + Afliver + Afkidney + Aflung + Afven + Afart + Afthyroid + Afrest + Afbrain]
       
       # melt the data to ggplot 
-      out.m <- reshape2::melt(out, id.vars = c("time"), 
+      out.m <- melt.data.table(out, id.vars = c("time"), 
                               variable.name = 'tissue', 
                               value.name = return.units)
       setDT(out.m)
       out.m[, model := '1st trimester']
       out.m[time > 91, model := '2nd-3rd trimester']
-      # out.m[, body := "maternal"]
-      # out.m[tissue %in% colnames(out)[grep("^Af", colnames(out))], body := "fetal"]
-      # out.m[tissue %in% c("Aplacenta", "Aconceptus"), body := "fetal"]
-      # out.m[, tissue := sub("^A[f]*", "", tissue)]
-      # out.m[tissue == "ftotal", body := "fetal"]
-      # out.m[tissue == "ftotal", tissue := "total"]
-      # out.m[tissue == "Mtotal", tissue := "total"]
       
-      # plot all compartments on a graph faceted by mother/fetus 
-      # all.tissues <- c(union(maternal_compts, fetal_compts), "conceptus", "placenta") 
-      # set3.colors <- brewer.pal(12, 'Set3') # max num of colors for Set 3 is 12 
-      # tissue.colors <- setNames(set3.colors, all.tissues[all.tissues != "gutlumen"])
-      # tissue.colors <- c(tissue.colors, "#7FC97F", "#E41A1C", "#FF7F00", "#000000")
-      # names(tissue.colors)[13:length(tissue.colors)] = c("gutlumen", "metabolized", "tubules", "total")
-      
-      p <- ggplot(out.m[!is.na(get(return.units))], aes(x = time, y = log10(get(return.units)))) + 
+      p <- ggplot(out.m[!is.na(get(return.units))], 
+                  aes(x = time, y = log10(get(return.units)))) + 
         geom_point(aes(color = model)) +
-        scale_color_manual(values = c('1st trimester' = 'red', '2nd-3rd trimester' = 'black')) +
+        scale_color_manual(values = c('1st trimester' = 'red', 
+                                      '2nd-3rd trimester' = 'black')) +
         facet_wrap(~tissue) + 
         theme_bw() + 
-        # theme(title = element_text(size = 16),
-        #       axis.title.x = element_text(size = 16),
-        #       axis.title.y = element_text(size = 16), 
-        #       axis.text.y = element_text(size = 13), 
-        #       legend.text = element_text(size = 13), 
-        #       legend.title = element_text(size = 13), 
-        #       strip.text = element_text(size = 13)) + 
         labs(x = 'time (days)', y = 'Amount (log10 umol)', 
              title = 'Chemical Amounts in Compartments of full gestational model') +
-        # xlim(0,300) +
         guides(colour = guide_legend(override.aes = list(size = 5)))
       
       print(p)
@@ -206,38 +173,18 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
       setDT(out)
       
       # melt the data to ggplot 
-      out.m <- reshape2::melt(out, id.vars = c("time"), 
+      out.m <- melt.data.table(out, id.vars = c("time"), 
                               variable.name = 'tissue', 
                               value.name = return.units)
       setDT(out.m)
-      # out.m[, body := "maternal"]
-      # out.m[tissue %in% colnames(out)[grep("^Cf", colnames(out))], body := "fetal"]
-      # out.m[tissue %in% c("Cconceptus", "Cplacenta"), body := "conceptus"]
-      # out.m[, tissue := sub("^C[f]*", "", tissue)]
-      # 
-      # # plot all compartments on a graph faceted by mother/fetus 
-      # all.tissues <- c(union(maternal_compts, fetal_compts), "conceptus", "placenta") 
-      # set3.colors <- brewer.pal(12, 'Set3') # max num of colors for Set 3 is 12 
-      # tissue.colors <- setNames(set3.colors, all.tissues[all.tissues != "gutlumen"])
-      # tissue.colors <- c(tissue.colors, "#7FC97F")
-      # names(tissue.colors)[13] = c("gutlumen")
       
       p <- ggplot(out.m[!is.na(get(return.units))], 
                   aes(x = time, y = log10(get(return.units)))) + 
         geom_point(aes(color = tissue)) +
-        # scale_color_manual(values = tissue.colors) +
         facet_wrap(~body) + 
         theme_bw() + 
-        # theme(title = element_text(size = 16),
-        #       axis.title.x = element_text(size = 16),
-        #       axis.title.y = element_text(size = 16), 
-        #       axis.text.y = element_text(size = 13), 
-        #       legend.text = element_text(size = 13), 
-        #       legend.title = element_text(size = 13), 
-        #       strip.text = element_text(size = 13)) + 
         labs(x = 'time (days)', y = 'Concentration (log10 uM)', 
              title = 'Chemical Concentration in Compartments of full gestational model') +
-        # xlim(0,300) +
         guides(colour = guide_legend(override.aes = list(size = 5)))
       
       print(p)
@@ -251,7 +198,7 @@ full_pregnancy <- function(dtxsid, track.vars = NULL, plt = FALSE,
   if (is.null(track.vars)) {
     
     # have the default output columns be selected concs 
-    default.track.vars <- c("Agutlumen",maternal_concs,
+    default.track.vars <- c("Agutlumen", maternal_concs,
                               "Aconceptus", "Cconceptus",
                               "Cplasma",
                               "Atubules","Ametabolized","Rblood2plasma",
