ef3f2381b619ca1d7d2a0319837bf331b423cea8
Kimberly Truong <kttruong258@gmail.com>
2025-03-10 16:38:35 -0400

    ToxValDB comparison update- add physchem.exclude = FALSE, more chems included, and add pre-filtering step for mc5 curves

--- scripts/devtox_pods.R
+++ scripts/devtox_pods.R
@@ -4,7 +4,7 @@
 # 
 # @author: Kimberly Truong 
 # created: 12/7/23
-# updated: 12/11/24
+# updated: 3/10/2025
 # ==============================================================================
 
 rm(list=ls())
@@ -18,7 +18,7 @@ library(ggrepel)
 library(latex2exp)
 library(cowplot)
 
-load('./data/invitrodb_v3_5_deiod_filtered_httk_121024.RData', 
+load('./data/invitrodb_v3_5_deiod_filtered_httk.RData', 
      verbose = TRUE)
 
 targets <- c("DIO1", "DIO2", "DIO3", "IYD")
@@ -28,7 +28,7 @@ pdata <- tissue.bers[variable == 'aed', .(dtxsid, chnm, variable, value)]
 pdata[, min_aed := min(value), by = .(dtxsid)]
 pdata <- pdata[order(dtxsid)]
 
-# take the min AED for each of the 98 chems 
+# take the min AED for each of the 103 chems 
 pdata2 <- unique(pdata[, .(dtxsid, chnm, min_aed)])
 
 # add in PODs from ToxValDB, if available 
@@ -37,9 +37,9 @@ toxval <- toxval %>% filter(dtxsid %in% pdata2$dtxsid)
 pdata2$pod <- log10(toxval$pod[match(pdata2$dtxsid, toxval$dtxsid)])
 pdata3 <- pdata2[which(!is.na(pdata2$pod)), ] 
 
-# 62 chems with complementary ToxValDB POD
+# 66 chems with complementary ToxValDB POD
 pdata3[, length(unique(dtxsid))]
-#> [1] 62
+#> [1] 66
 
 pod.min <- min(pdata3$min_aed, pdata3$pod)
 pod.max <- max(pdata3$min_aed, pdata3$pod)
@@ -61,21 +61,22 @@ toxval.pp <- ggplot(data = pdata3, aes(x = min_aed, y = pod)) +
   geom_label_repel(aes(label = chnm), 
                    data = pdata3[abs(pod - min_aed) > 1],
                    size = 4, 
-                   force = 40, max.overlaps = 9) +
+                   force = 50, max.overlaps = 9) +
   my_theme +
-  scale_x_continuous(breaks = seq(-4, pod.lims[2], 1), 
+  scale_x_continuous(breaks = seq(pod.lims[1], pod.lims[2], 1), 
                      limits = pod.lims) +
-  scale_y_continuous(breaks = seq(-3, pod.lims[2], 1), 
+  scale_y_continuous(breaks = seq(pod.lims[1], pod.lims[2], 1), 
                      limits = pod.lims) +
   labs(x = 'min DIO/IYD-bioactivity-based AED (log10 mg/kg/day)', 
        y = 'ToxVal POD (log10 mg/kg/day)')
 
-# interesting chems above the red line 
+# interesting chems above grey line 
 pdata3 %>% 
   filter(min_aed + 1 < pod) %>% 
   select(dtxsid, chnm) %>% print()
 
 chems.to.investigate <- pdata3[min_aed + 1 < pod, dtxsid]
+# print source(s) underlying ToxValDB pod 
 toxval.studies <- toxval %>% filter(dtxsid %in% chems.to.investigate) %>% 
   select(name, studies, pod_10, pod_hra, pod_hra_source, pod) %>% print()
 
@@ -83,7 +84,7 @@ toxval.studies <- toxval %>% filter(dtxsid %in% chems.to.investigate) %>%
 
 # focus on the prioritized chems 
 dio.aeds <- tissue.bers[variable == 'aed' & min_ber <= 3, 
-                    .(dtxsid, chnm, enzyme, variable, body, tissue, value, min_ber)]
+                    .(dtxsid, chnm, enzyme, variable, compt, value, min_ber)]
 dio.aeds[, min_aed_bytarget := min(value), by = .(dtxsid, enzyme)]
 dio.aeds <- dio.aeds[order(dtxsid)]
 
@@ -102,13 +103,13 @@ priority.chems <- dio.aeds[, unique(dtxsid)]
 load("./data/invitrodb_v3_5_snapshot.RData", verbose = TRUE)
 
 # filter down to prioritized chems with BER < 3 
-mc5.dio.priority <- mc5[dsstox_substance_id %in% priority.chems]
+mc5.dio.priority <- mc5[dsstox_substance_id %in% priority.chems][use.me == 1]
 
 # take the 5%-tile AC50 for each chem 
 mc5.dio.priority[hitc == 1, modl_ga.5percentile := quantile(modl_ga, c(0.05), type = 2), 
                  by = dsstox_substance_id]
-toxcast.pods <- mc5.dio.priority[hitc == 1, .(dsstox_substance_id, chnm, aeid, aenm, 
-                                              modl_ga, modl_ga.5percentile)]
+toxcast.pods <- mc5.dio.priority[hitc == 1, .(dsstox_substance_id, chnm, aeid, aenm,
+                                              modl,modl_ga, modl_ga.5percentile)]
 toxcast.pods <- toxcast.pods[order(dsstox_substance_id)]
 toxcast.pods.httk <- unique(toxcast.pods[, .(dsstox_substance_id, chnm, 
                                              ac50_5percentile = modl_ga.5percentile)])
@@ -128,7 +129,8 @@ for (i in 1:nrow(toxcast.pods.httk)) {
                      daily.dose = 1, 
                      which.quantile = c(0.5, 0.95), 
                      model = '3compartmentss', 
-                     output.units = 'uM')
+                     output.units = 'uM', 
+                     parameterize.arg.list=list(physchem.exclude=FALSE))
   
   toxcast.pods.httk$mc.css50[i] <- out["50%"]
   toxcast.pods.httk$mc.css95[i] <- out["95%"]
@@ -137,7 +139,6 @@ for (i in 1:nrow(toxcast.pods.httk)) {
 
 toxcast.pods.httk[, `:=` (aed50 = lowerbd.ac50_uM/mc.css50, 
                           aed95 = lowerbd.ac50_uM/mc.css95)]
-
 setnames(toxcast.pods.httk, old = "dsstox_substance_id", new = "dtxsid")
 
 # melt the data for ggplot
@@ -217,7 +218,7 @@ bioactive.pods <- dxp + geom_segment(data = toxcast.pods.httk,
                                   y = toxcast.pods.httk$aed95, 
                                   xend = toxcast.pods.httk$chnm, 
                                   yend = toxcast.pods.httk$aed50), 
-                              color = "#9E9E9E", size = 1)
+                              color = "#9E9E9E", linewidth = 1)
 
 # put it altogether in one figure
 pods.fig <- plot_grid(toxval.pp, bioactive.pods, 
@@ -231,12 +232,12 @@ ggsave(plot = pods.fig,
        dpi = 300, 
        width = 18.53, height = 8.32, 
        device = "tiff", 
-       filename = "./figures/300dpi/bioactivity_pods-v2.tiff")
+       filename = "./figures/300dpi/bioactivity_pods-v3.tiff")
 
 ggsave(plot = pods.fig, 
        units = "in", 
        dpi = 300, 
        width = 18.53, height = 8.32, 
        device = "png", 
-       filename = "./figures/bioactivity_pods-v2.png")
+       filename = "./figures/bioactivity_pods-v3.png")
 
